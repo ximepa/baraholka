e3034065aa44b9ab782f4dda9f3bef9969bc9f8e
ximepa <ximepa1@yandex.ru>
2015-01-06 13:33:03 +0200

    add user model

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,7 +5,6 @@ class Users extends CI_Controller {
     public function index()
     {
         $this->load->model('users_model');
-        $this->users_model->get_last_ten_entries();
         $data['query'] = $this->users_model->get_last_ten_entries();
         $this->load->view('users_view', $data);
     }
@@ -13,9 +12,9 @@ class Users extends CI_Controller {
     public function user_add()
     {
         $this->load->model('users_model');
-        $this->users_model->get_last_ten_entries();
+        $this->load->helper('form');
         $data['query'] = $this->users_model->user_add();
-        $this->load->view('users_view', $data);
+        $this->load->view('user_add_view', $data);
     }
 }
 
